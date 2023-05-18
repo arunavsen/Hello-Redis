cc393013dfbf1f3c48e8db57d235f9ad8892c2f2
arunav <arunav@ARUNAV-PC>
2023-05-18 16:34:12 +0600

    Add DashboardData model

---
+++ Models/DashboardData.cs
@@ -0,0 +1,11 @@
+﻿namespace RedisDemo.Models
+{
+    [Serializable]
+    public class DashboardData
+    {
+        public int TotalCustomerCount { get; set; }
+        public int TotalRevenue { get; set; }
+        public string TotalSpellingProductName { get; set; }
+        public string TotalSpellingCountryName { get; set; }
+    }
+}
