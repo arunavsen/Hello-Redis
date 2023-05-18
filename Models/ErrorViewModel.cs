22b5c4bddb6930a92adb7671dfe8170aa09d2a69
arunav <arunav@ARUNAV-PC>
2023-05-18 14:08:38 +0600

    Remove unnecessary codes

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RedisDemo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
