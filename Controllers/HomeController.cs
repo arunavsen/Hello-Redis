ad4362012202ddbfb496997939354b238032c82b
arunav <arunav@ARUNAV-PC>
2023-05-18 16:57:39 +0600

    Add SaveCacheData Action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using RedisDemo.Models;
 using System.Diagnostics;
 
@@ -6,6 +8,31 @@ namespace RedisDemo.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private IDistributedCache _distributedCache;
+        public HomeController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;  
+        }
+        public async Task<ActionResult> SaveRedisCache()
+        {
+            var dashBoardData = new DashboardData()
+            {
+                TotalCustomerCount = 1004050,
+                TotalRevenue = 120050,
+                TotalSpellingCountryName = "Bangladesh",
+                TotalSpellingProductName = "Xiomi"
+            };
+
+            var tomorrow = DateTime.Now.Date.AddDays(1);
+            var totalSeconds = tomorrow.Subtract(DateTime.Now).TotalSeconds;
+
+            var distributedCacheEntryOptions = new DistributedCacheEntryOptions();
+            distributedCacheEntryOptions.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(totalSeconds);
+            distributedCacheEntryOptions.SlidingExpiration = null;
+
+            var jsonData = JsonConvert.SerializeObject(dashBoardData);
+            await _distributedCache.SetStringAsync("DashboardData", jsonData, distributedCacheEntryOptions);
+            return View();
+        }
     }
 }
